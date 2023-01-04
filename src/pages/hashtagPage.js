bee89631ac5f16b337a0c8353d211c896622501c
Victor Leone de Oliveira <vileoleone@gmail.com>
2023-01-04 19:02:27 -0300

    feat: static page with basic components: trending box and header

--- src/pages/hashtagPage.js
+++ src/pages/hashtagPage.js
@@ -1,10 +1,22 @@
 import Header from "../components/Header.js"
-import MainBody from "../components/MainBody.js"
+import styled from "styled-components"
+import TrendingBox from "../components/TrendingBox.js"
 export function HashTagPage() {
     return (
         <>
-        <Header />
-        <MainBody></MainBody>   
+            <Header />
+            <BackgroundBody>
+                <TrendingBox/>
+            </BackgroundBody>
         </>
     )
 }
+
+const BackgroundBody = styled.main`
+    background-color: #E5E5E5;
+    display: flex;
+    width: 100%;
+    height: 100vh;
+    position: relative;
+    z-index: 0;
+`
