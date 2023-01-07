9aa5b8b036e7dfb01d1a84ab2dc3f87d9cdcb696
Pedro <malpvpandtextures@gmail.com>
2023-01-07 20:43:39 -0300

    post bug fix

--- src/pages/TimelinePage.js
+++ src/pages/TimelinePage.js
@@ -12,21 +12,22 @@ export default function Timeline() {
 
     const [showPosts, setShowPosts] = useState([])
     const [loading, setLoading] = useState(true)
+    const [resetPage,setResetPage] = useState(0)
     useEffect(() => {
         const promise = axios.get(`https://linkr-api-jt7z.onrender.com/timeline`)
-        promise.then(res => setShowPosts(res.data) & setLoading(false)
+        promise.then(res => setShowPosts(res.data) & setLoading(false)&console.log(res.data)
         )
         promise.catch(erro => console.log(erro) & alert('An error occured while trying to fetch the posts, please refresh the page')
         )
 
-    }, [])
+    }, [resetPage])
 
     return (
         <Container>
             <Header />
             <TimelineBody>
                 <h1>timeline</h1>
-                <TrendingBox />
+                <TrendingBox  setResetPage={setResetPage}/>
                 <MkPosts />
                 <InnerContainer>
                     {loading ? <h3>Loading <AiOutlineLoading3Quarters /></h3> : showPosts.length === 0 ? <h3>There are no posts yet</h3> : showPosts.map((e, i) => <Posts key={i} name={e.name} description={e.description} />)}
