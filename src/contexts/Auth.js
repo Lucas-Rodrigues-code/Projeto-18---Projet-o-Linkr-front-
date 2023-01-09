9aa4decec2d8df09fcf2cc61c91fd69afa920a6c
Pedro <malpvpandtextures@gmail.com>
2023-01-08 22:47:57 -0300

    bug fix posts

--- src/contexts/Auth.js
+++ src/contexts/Auth.js
@@ -1,14 +1,13 @@
 import React, { createContext, useState } from "react";
-
 export const AuthContext = createContext({})
 
 function AuthProvider({ children }) {
     const token = localStorage.getItem("token"); 
-    
+    const [myToken,setMytoken] = useState()
     console.log(token); 
 
     return (
-        <AuthContext.Provider value={{token}}>
+        <AuthContext.Provider value={{token,myToken,setMytoken}}>
             {children}
         </AuthContext.Provider>
     )
